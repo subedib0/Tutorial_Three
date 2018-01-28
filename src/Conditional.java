c86c08ede0c150988ae12579f36716c2342e3be0
subedib0 <subedib0@gmail.com>
2018-01-28 15:11:21 -0500

    Conditions Practice

---
+++ src/Conditional.java
@@ -0,0 +1,34 @@
+public class Conditional {
+
+    public static void main(String[] args) {
+       /* int number1=3;
+        int number2 =5;
+        //= means to assign
+        //==means check the equality
+        if(number1==number2){
+            System.out.println("number one is equeal to number two");
+
+
+        }
+        else if (number1>number2){
+            System.out.println("number one is greater than number two");
+        }
+        else{
+            System.out.println("Number is less than number two");
+        }*/
+
+        String power = "off";
+        switch (power) {
+            case ("off"):
+                System.out.println("power is off");
+                break;
+            case ("on"):
+                System.out.println("the power is on");
+                break;
+
+
+
+
+        }
+    }
+}
